d2447b07f50cef44d1d3f9e96afcba00d884430e
Alexander Shah <AlexKShah@gmail.com>
2018-02-10 15:37:25 -0500

    formatting

--- AlexShah-Parallel-Labs/src/lab5/GLEstimator.scala
+++ AlexShah-Parallel-Labs/src/lab5/GLEstimator.scala
@@ -3,13 +3,13 @@ package lab5
 object GLEstimator extends App {
   val t0 = System.nanoTime()
 
-  println("Pi = " + (0 until Int.MaxValue).foldLeft(0.0) { (sum, n) =>
-    (sum + 4.0*(math.pow(-1,n))/((2.0*n)+1))
-  }
-  )
+  println("Pi = " +
+    (0 until Int.MaxValue).foldLeft(0.0) { (sum, n) =>
+      sum + 4.0 * math.pow(-1, n) / ((2.0 * n) + 1)
+    })
 
   val t1 = System.nanoTime()
-  val dt = (t1-t0)/1000000000.0
+  val dt = (t1 - t0) / 1000000000.0
 
   println("dt = " + dt)
 }
