8d89685dcf1ecbe85f6c13749d48e6cb57ca9ce4
Alexander Shah <AlexKShah@gmail.com>
2018-02-10 15:12:31 -0500

    lab 5 continued, working, but only as Double?

--- AlexShah-Parallel-Labs/src/lab5/GLEstimator.scala
+++ AlexShah-Parallel-Labs/src/lab5/GLEstimator.scala
@@ -3,11 +3,13 @@ package lab5
 object GLEstimator extends App {
   val t0 = System.nanoTime()
 
-  println("Pi = " + (0 until Int.MaxValue).foldLeft(0L) { (sum,n) => sum+((math.pow(-4,n).toLong)/(2L*n+1)) } )
+  println("Pi = " + (0 until Int.MaxValue).foldLeft(0.0) { (sum, n) =>
+    (sum + 4.0*(math.pow(-1,n))/((2.0*n)+1))
+  }
+  )
 
   val t1 = System.nanoTime()
-
-  val dt = t1-t0*1000000000
+  val dt = (t1-t0)/1000000000.0
 
   println("dt = " + dt)
 }
