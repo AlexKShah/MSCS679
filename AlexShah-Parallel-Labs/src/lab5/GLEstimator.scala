4bf3b1c7469538ee5ae35710608b0778a776472c
AlexkShah <alexkshah@gmail.com>
2018-02-15 19:27:11 -0500

    implement bitwise and

--- AlexShah-Parallel-Labs/src/lab5/GLEstimator.scala
+++ AlexShah-Parallel-Labs/src/lab5/GLEstimator.scala
@@ -2,11 +2,15 @@ package lab5
 
 object GLEstimator extends App {
   val t0 = System.nanoTime()
-  val pi = (0 until 99).foldLeft(0.0) { (sum, n) =>
-      sum + 4.0 * math.pow(-1, n) / ((2.0 * n) + 1)
-    }
+
+  val pi = (0 until Int.MaxValue).foldLeft(0.0) { (sum, n) =>
+    val one = if ((n&1)==0) 4 else -4
+    sum + one / ((2.0 * n) + 1)
+  }
+
   val t1 = System.nanoTime()
   val dt = (t1 - t0) / 1000000000.0
+
   println("Pi = " + f"$pi%2.2f")
   println("dt = " + f"$dt%2.2f")
 }
