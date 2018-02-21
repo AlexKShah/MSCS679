67ec38791bc3449ff240589b6ed5134b130f099e
AlexkShah <alexkshah@gmail.com>
2018-02-20 20:17:24 -0500

    version from lab

--- AlexShah-Parallel-Labs/src/lab6/ParallelGLEstimator.scala
+++ AlexShah-Parallel-Labs/src/lab6/ParallelGLEstimator.scala
@@ -10,34 +10,28 @@ object ParallelGLEstimator extends App {
   val MAX = Int.MaxValue
   val RANGE = 1000000L
   val numberPartitions = (MAX.toDouble / RANGE).ceil.toInt
-
-  def calcPi(low: Long, high: Long, max: Int): Double = {
-    var index: Long = low
-    var sum = 0.0
-    while(index <= high && high != MAX) {
-      val one = if ((index & 1) == 0) 4.0 else -4.0
-      sum += one / ((2.0 * index) + 1.0)
-      index += 1L
-    }
-    sum
-  }
   val t0 = System.nanoTime()
 
-  val futures = for (i <- 0L until numberPartitions) yield Future {
-    val low: Long = i * RANGE + 1
-    val high: Long = MAX.toLong min (i + 1 * RANGE)
-    calcPi(low, high, MAX)
+  val futures = for (k <- 0L until numberPartitions) yield Future {
+    val low: Long = k * RANGE + 1
+    val high: Long = MAX.toLong min (k + 1) * RANGE
+
+    val partialpi = (low to high).foldLeft(0.0) { (sum, n) =>
+      val one = if ((n & 1) == 0) 4 else -4
+      sum + one / ((2.0 * n) + 1)
+    }
+    partialpi
   }
 
-  val pi = futures.foldLeft(0.0) { (sum, future) =>
+  val totalpi = futures.foldLeft(4.0) { (sum, future) =>
     import scala.concurrent.duration._
     val result = Await.result(future, 100 seconds)
-    sum+result
+    sum + result
   }
 
   val t1 = System.nanoTime()
   val dt = (t1 - t0) / 1000000000.0
-  println("cores = " + (Runtime.getRuntime.availableProcessors())/2)
-  println("Pi = " + f"$pi%2.2f")
-  println("dt = " + f"$dt%2.2f")
+  println("cores = " + (Runtime.getRuntime.availableProcessors()) / 2)
+  println("Pi = " + totalpi)
+  println("dt = %6.2f".format(dt))
 }
